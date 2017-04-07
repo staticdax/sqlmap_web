7c32165caeea1e0b0769c12c032590c6e95d9b49
root <root@localhost.localdomain>
2017-04-06 21:51:42 -0400

    optimized

--- start_api_serv.sh
+++ start_api_serv.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "" > /var/www/sqlmap_web/api_serv.log
-sqlmapapi -s -H 127.0.0.1 | tee /var/www/sqlmap_web/api_serv.log & sleep 2 && chmod o+r /tmp/sqlmapipc-*
+sqlmapapi -s -H 127.0.0.1 | tee /var/www/sqlmap_web/api_serv.log & sleep 6 && chmod o+r /tmp/sqlmapipc-*
